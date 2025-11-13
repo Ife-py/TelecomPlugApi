3d3e6bd1c25fca776dfe06ab4c7707053b8215f9
ifeoluwa <omolekeifeoluwa2005@gmail.com>
2025-11-13 09:32:59 +0100

    update route for swagger

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+
+Route::get('/api-docs.json', function () {
+    $path = storage_path('api-docs/api-docs.json');
+    if (!File::exists($path)) {
+        abort(404, 'Swagger JSON not found');
+    }
+    return response()->file($path, [
+        'Content-Type' => 'application/json',
+    ]);
+});
+
 
 Route::get('/', function () {
     return redirect('/api/documentation');
