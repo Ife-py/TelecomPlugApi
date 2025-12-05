a8e830a230f4e1a87ed3725349e3a02fa9f105f6
ifeoluwa <omolekeifeoluwa2005@gmail.com>
2025-12-05 15:51:26 +0100

    update cors

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,7 @@ return [
     'allowed_origins' => [
         env('FRONTEND_URL', 'http://localhost:3000'),
         'http://localhost:5173',
-        'https://telecom-front.vercel.app/',],
+        'https://telecom-front.vercel.app',],
 
     'allowed_origins_patterns' => [],
 
