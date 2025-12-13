1d254e8ee924f57ce8ce6263fb5e7f333d7f2b05
ifeoluwa <omolekeifeoluwa2005@gmail.com>
2025-12-13 22:02:17 +0100

    update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,15 +12,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-
-        // ✅ Pure API middleware (NO SPA, NO CSRF)
-        $middleware->api();
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
+
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })
-    ->create();
+    })->create();
